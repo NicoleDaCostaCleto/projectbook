cbb7827f7bc6c488c87f72f28e65b33ca8689ba2
Nicole Da Costa <shax444@protonmail.com>
2022-07-22 10:27:19 +0200

    dark mode

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,25 @@
+const body=document.querySelector('.jsbody');
+// j'affecte la class de mon button à nightToggleBtn
 const nightToggleBtn = document.querySelector('.js-night-toggle');
 
-nightToggleBtn.addEventListener('click', function() {
-    const body=document.querySelector('.jsbody');
+// je controlle si le dark mode est activé dans le local storage,
+// si oui je l'active avec le CSS avec la class night-activated
+if (localStorage.getItem('nightActivated') === 'true') {
+    body.classList.add('night-activated');
+    // Je change l'affichage de mon button
+    nightToggleBtn.innerText = "light Mode";
+}
 
+nightToggleBtn.addEventListener('click', function() {
+    // si le mode dark est activé, je le désactive
     if (body.classList.contains('night-activated')) {
         body.classList.remove('night-activated');
-    }else {
+        localStorage.removeItem('nightActivated');
+        nightToggleBtn.innerText = "night Mode";
+    // si le mode dark n'est pas activé, je l'active
+    } else {
         body.classList.add('night-activated');
-    }});
+        localStorage.setItem('nightActivated', "true");
+        nightToggleBtn.innerText = "light Mode";
+    }
+});
